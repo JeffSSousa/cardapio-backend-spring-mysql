71d0972cf5da1746105bb95df3c7423427740cbc
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-07-25 11:12:16 -0300

    feat: implements Food entity

---
+++ src/main/java/com/jeffersonsousa/cardapio/entity/Food.java
@@ -0,0 +1,67 @@
+package com.jeffersonsousa.cardapio.entity;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "foods")
+@Entity(name = "foods")
+public class Food {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String image;
+	private Integer price;
+	
+	public Food() {
+	}
+
+	public Food(Long id, String title, String image, Integer price) {
+		this.id = id;
+		this.title = title;
+		this.image = image;
+		this.price = price;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Food other = (Food) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+
+}
