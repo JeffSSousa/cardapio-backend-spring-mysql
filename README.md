6546a075d6f1eeef7c9debc09c1ea5d9a609a9a7
Jefferson S Sousa <125930065+JeffSSousa@users.noreply.github.com>
2025-07-25 10:50:52 -0300

    feat: create Project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Create Project Back End cardapio
---
+++ src/test/java/com/jeffersonsousa/cardapio/CardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeffersonsousa.cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
